cb6803dff4b5ed0eb90ead05f049af01eb8702e6
yjc133 <yjc133@naver.com>
2021-01-19 18:58:38 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')  # respond 헤더 추가
+        self.end_headers()
+        f = open('data.html')
+        self.wfile.write(bytes(f.read(), 'utf8'))
+        f.close()
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
